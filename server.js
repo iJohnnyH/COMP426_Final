417b758e95e978e618a0ff5baf44025e463e1e6d
ijohnnyh <ijohnnyh@live.unc.edu>
2017-12-04 00:13:19 -0500

    Added multer for upload picture support

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var multer = require('multer')
+
+global.__rootdir = __dirname
+const app = express()
+
+var upload = multer({
+	destination: '__rootdir/imgs/',
+	filename: 'test'
+})
 
 app.get('/', function(req,res){
-	res.sendFile('html/main.html', { root: __dirname });
+	res.sendFile('public/html/puzzle.html', { root: __dirname })
 })
+
+app.post('/api/upload', upload.single('pic'), (req, res) => {
+	if (!req.file) {
+	  console.log("No file received");
+	  return res.send({
+		success: false
+	  });
+  
+	} else {
+	  console.log('file received');
+	  return res.send({
+		success: true
+	  })
+	}
+  });
 
 app.listen(8000,function(){
 	console.log('Running on localhost:8000');
