2dfb0c14e5bf14ed86985760396b84a2e2764d0b
ijohnnyh <ijohnnyh@live.unc.edu>
2017-12-10 18:37:01 -0500

    dynamic select image

---
+++ public/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var select = document.getElementById('picSelect');
+	//var options = select.innerHTML;
+	select.innerHTML = "<option value = \'Test\'>Test</option>";
+	//options = '<option value=\"\" disabled selected>Select an image</option>'
+}
