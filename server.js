b924d80d2610fbf18cd4450674ca9af253c169cb
ijohnnyh <ijohnnyh@live.unc.edu>
2017-12-10 16:34:24 -0500

    Uploads filepath to db

--- server.js
+++ server.js
@@ -1,42 +1,72 @@
 var express = require('express')
 var multer = require('multer')
 var path = require('path')
-var mongo = require('mongodb')
+var mongoose = require('mongoose')
 
 const app = express()
 
 var storage = multer.diskStorage({
 	destination: function (req, file, callback) {
-	  callback(null, 'public/images/')
+		callback(null, 'public/images/')
 	},
 	filename: function (req, file, callback) {
 	  callback(null, file.originalname)
 	}
-  })
+})
 
 var upload = multer({storage: storage})
 
+//Schema and model for database
+var pictureSchema = mongoose.Schema({
+	filepath: String
+})
+var Picture = mongoose.model('Picture',pictureSchema)
+
+//Connect to mongodb
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('Connected to database')
+});
+
+//API Routes
+
+//Homepage
 app.get('/', function(req,res){
 	res.sendFile('public/html/puzzle.html', { root: __dirname })
 })
 
+//Upload button
 app.post('/api/upload', upload.single('pic'), (req, res) => {
 	if (!req.file) {
 		onsole.log("No file received");
 		return res.send({success: false});
 	} else {
+
 		console.log('File received')
 		console.log(req.file)
 		console.log(req.file.originalname)
+
+		//Save to db
+		var newImage = new Picture({
+			filepath: req.file.originalname
+		});
+		newImage.save(function (err, comment){
+			if (err) console.log(error);
+			else console.log('Saved to db succesfully:', comment)
+		})
 		return res.send({success: true})
 	}
 });
 
+//Login page
 app.get('/login', function(req,res){
 	res.sendFile('public/html/login.html', { root: __dirname })
-	
 })
 
+
+//Start app
 app.listen(8000,function(){
 	console.log('Running on localhost:8000');
 })
