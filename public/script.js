4cf3ac5d7ea8d66f9091cab0e7bcf4205dea5ad9
ijohnnyh <ijohnnyh@live.unc.edu>
2017-12-11 14:34:30 -0500

    deleted cus worthless

--- public/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-	var select = document.getElementById('picSelect');
-	//var options = select.innerHTML;
-	select.innerHTML = "<option value = \'Test\'>Test</option>";
-	//options = '<option value=\"\" disabled selected>Select an image</option>'
-}
