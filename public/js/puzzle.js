f9c33a34e71258549ca14abf67a3bd004d451524
Dennis <dsun96@gmail.com>
2017-12-13 12:51:27 -0500

    high score popup is in init(), copy and paste into gameover()

--- public/js/puzzle.js
+++ public/js/puzzle.js
@@ -25,6 +25,28 @@ function init(){
 	var select = document.getElementById('picSelect');
 	imgPath = select.value;
 	img.src = "../images/" + imgPath;
+    
+    //GameOver popup (modal) stuff
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // opens up the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
 }
 
 function initGame(path){
